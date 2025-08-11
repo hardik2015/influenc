445ac0763a1bc47044d4d02b1ec217d71e0e7606
Hardik Viradiya <43150887+hardik2015@users.noreply.github.com>
2025-08-11 20:02:00 +0530

    Update comyui_prompt_api.py

--- comyui_prompt_api.py
+++ comyui_prompt_api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import os, json, sys
 from threading import Thread
 from pathlib import Path
-from imagekitio import ImageKit
+from imagekitio import ImageKit, UploadFileRequestOptions
 import traceback
 
 # ——— CONFIG ———
