12732e7343553d1a502acd7294d0ac36f118841b
Hardik Viradiya <43150887+hardik2015@users.noreply.github.com>
2025-08-21 15:20:35 +0530

    Update comyui_prompt_api.py

--- comyui_prompt_api.py
+++ comyui_prompt_api.py
@@ -10,7 +10,7 @@ import traceback
 COMFY_API_BASE = os.getenv("COMFY_API_BASE", "http://localhost:8188/api")
 COMFY_API_PROMPT = f"{COMFY_API_BASE}/prompt"
 COMFY_API_QUEUE = f"{COMFY_API_BASE}/queue"
-PROMPT_NODE_ID = "163"
+PROMPT_NODE_ID = "558"
 UPLOAD_FOLDER = "../ComfyUI/output/"
 HOST = "0.0.0.0"
 PORT = 5010
